7161779b0a5b1aa03649aa429a139dc4e7326c4a
nohsangwoo <nsgr12@naver.com>
2020-10-28 23:08:20 +0900

    3.2 mapStateToProps

--- src/route/Home.js
+++ src/route/Home.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
 
-function Home() {
+function Home(props) {
+  console.log(props);
+  const { toDos } = props;
   const [text, setText] = useState("");
 
   function onChange(e) {
@@ -9,7 +12,8 @@ function Home() {
 
   function onSubmit(e) {
     e.preventDefault();
-    console.log(text);
+    // console.log(text);
+    setText("");
   }
   return (
     <>
@@ -18,9 +22,16 @@ function Home() {
         <input type="text" value={text} onChange={onChange} />
         <button>add</button>
       </form>
-      <ul></ul>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 }
 
-export default Home;
+// 첫번째 argument(state)는 store.js의 state값이고
+// 두번째 argument는 (ownProps) component의 props임 여기는 App.js에서  react-router-dom에의해 넘겨받은 props를 말함
+function getCurrentState(state, ownProps) {
+  return { toDos: state };
+}
+
+// connect store.js에 있는 data를 현재 Home.js로 연결해서 사용가능하게 해줌
+export default connect(getCurrentState)(Home);
